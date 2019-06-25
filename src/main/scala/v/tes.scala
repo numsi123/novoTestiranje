d2ec23def1b5c7747cefc8cd8822baf613f92c5f
numsi123 <cage.warp@gmail.com>
2019-06-25 21:14:31 +0200

    cistenje na glupostite na ungit

--- src/main/scala/v/tes.scala
+++ src/main/scala/v/tes.scala
@@ -3,14 +3,11 @@ package v
 object tes {
     // turame novo nesto pak
 
-<<<<<<< HEAD
-
-
     // uste master
     // nesto dodavame// nesto briseme
-=======
+
   // ova e za branh
   // vtoro na branch
   // ajde da pushame na branch
->>>>>>> granka
+
 }
