167512ed00caa96bab64f7ec298819e3bf6f10ff
numsi123 <cage.warp@gmail.com>
2019-06-25 21:35:33 +0200

    reverting back before sprej branch

--- src/main/scala/v/tes.scala
+++ src/main/scala/v/tes.scala
@@ -13,7 +13,10 @@ object tes {
   // vtoro na branch
   // ajde da pushame na branch
 
+<<<<<<< HEAD
   // ovde sprej ke stoi///////////
   val sprej = 10
 
+=======
+>>>>>>> parent of 5bf7bfd... val master 2
 }
